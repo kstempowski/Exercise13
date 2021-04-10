742b223843cba6f8e9019f6c385c7b84e93e2c63
Your Name <kennystemper@gmail.com>
2021-04-10 15:17:25 -0400

    Exercise 13

---
+++ Exercise13/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Exercise13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercise 13");
+            bool repeat = true;
+            while (repeat)
+            {
+                //Prompt the user for input
+                Console.WriteLine("Please enter a number greater than 0");
+                string input = Console.ReadLine();
+                int number = int.Parse(input);
+
+                for (int i = 0; number >= i; number--)
+                {
+                    Console.WriteLine(number);
+                }
+                //prompt the user for additional input
+                Console.WriteLine("Enter another number? y/n");
+                string answer = Console.ReadLine();
+
+                if (answer == "y")
+                {
+                    //An answer if "y" will repeat the program
+                    repeat = true;
+                }
+                else
+                {
+                    //Any answer other than "y" will terminate the program
+                    Console.WriteLine("Goodbye!");
+                    repeat = false;
+                }
+            }
+        }
+    }
+}
